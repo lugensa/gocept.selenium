f9eae661f438ea750dcb53c77feae7b2098533d6
Marius Gedminas <marius@gedmin.as>
2009-12-18 13:49:39 +0000

    Preparing release 0.2.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.selenium',
-    version = '0.3dev',
+    version = '0.2.1',
     author='Wolfgang Schnerring',
     author_email='ws@gocept.com',
     description='zope.testing layer that integrates Selenium-RC',
