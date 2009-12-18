901b3a3092a73f14a4d1c7d965bb7c3b7f7b0397
Marius Gedminas <marius@gedmin.as>
2009-12-18 13:50:32 +0000

    Back to development: 0.2.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.selenium',
-    version = '0.2.1',
+    version = '0.2.2dev',
     author='Wolfgang Schnerring',
     author_email='ws@gocept.com',
     description='zope.testing layer that integrates Selenium-RC',
