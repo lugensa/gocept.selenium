d0b40f87567834443ea19ebfae1342b9a6c54da5
Thomas Lotze <tl@gocept.com>
2010-05-20 08:35:58 +0000

    Preparing release 0.4.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.selenium',
-    version = '0.4.2dev',
+    version = '0.4.2',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     description='zope.testing layer that integrates Selenium-RC',
