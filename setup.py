7562697f17d533fd7b8ec272639309c4236ee3c8
Wolfgang Schnerring <wosc@wosc.de>
2010-08-16 08:37:34 +0000

    Preparing release 0.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.selenium',
-    version='0.7dev',
+    version='0.7',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     description='zope.testing layer that integrates Selenium-RC',
