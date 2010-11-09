19a006e03a0040a52312dafdd765261867c38ba9
Jan-Jaap Driessen <jdriessen@thehealthagency.com>
2010-11-09 15:43:01 +0000

    gocept.selenium.wsgi allows you to plug in a wsgi app for testing

---
+++ src/gocept/selenium/wsgi/testing.py
@@ -0,0 +1,42 @@
+#############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+def simple_app(environ, start_response):
+    path = environ['PATH_INFO']
+
+    statuscode = '404 Not Found'
+    body = 'Not Found'
+    headers = []
+    if path == '/':
+        statuscode = '200 OK'
+        headers.append(('Content-Type', 'text/html'))
+        body = '''
+          <html>
+          <head>
+            <script src="colors.js"></script>
+          </head>
+          <body>
+            <p id="foo">Testing...</p>
+          </body>
+          </html>'''
+    elif path == '/colors.js':
+        statuscode = '200 OK'
+        headers.append(('Content-Type', 'text/javascript'))
+        body = '''
+        var hello = function hello () {
+            document.getElementById('foo').innerHTML = 'Hello from javascript';
+        };
+        window.onload = hello;
+        '''
+    start_response(statuscode, headers)
+    return body
