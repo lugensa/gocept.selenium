f945059e5d7ac91a9dc17669e35b34843c6a5946
Wolfgang Schnerring <wosc@wosc.de>
2010-12-07 14:47:19 +0000

    Get a new ZODB connection per traversal

--- src/gocept/selenium/zope2/__init__.py
+++ src/gocept/selenium/zope2/__init__.py
@@ -14,7 +14,11 @@
 
 import Lifetime
 import Testing.ZopeTestCase
+import Testing.ZopeTestCase.ZopeLite
+import Testing.ZopeTestCase.connections
+import Testing.ZopeTestCase.sandbox
 import Testing.ZopeTestCase.utils
+import Zope2
 import gocept.selenium.base
 import random
 import time
@@ -73,3 +77,27 @@ class TestCase(gocept.selenium.base.TestCase,
                Testing.ZopeTestCase.FunctionalTestCase):
 
     layer = Layer(*BASE_LAYERS)
+
+    def _app(self):
+        # Testing.ZopeTestCase.Sandbox handling of the DemoStorage change is a
+        # little... crude:
+        #
+        # ZApplicationWrapper is instantiated with a DB from
+        # Testing/custom_zodb, but that is never used later on, but Sandbox
+        # passes in the connection (to the current DB) to use instead.
+        # While this is fine for "normal" tests (using testbrowser or whatnot),
+        # this means that there only ever is one single ZODB connection, even
+        # among multiple threads -- which clearly is not what we want.
+        #
+        # Thus, this rewrite of the upstream method, that properly changes the
+        # DB in ZApplicationWrapper, yielding a new connection upon each
+        # traversal.
+        Zope2.startup()
+        db, aname, version = Zope2.bobo_application._stuff
+        db = Testing.ZopeTestCase.ZopeLite.sandbox()
+        Zope2.bobo_application._stuff = db, aname, version
+        app = Zope2.bobo_application()
+        Testing.ZopeTestCase.sandbox.AppZapper().set(app)
+        app = Testing.ZopeTestCase.utils.makerequest(app)
+        Testing.ZopeTestCase.connections.register(app)
+        return app
