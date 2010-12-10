859ddd544cde01a7364f0b8c6908922065702851
Jan-Jaap Driessen <jdriessen@thehealthagency.com>
2010-12-10 13:12:07 +0000

    A test for multiple WSGI layers.

--- src/gocept/selenium/wsgi/tests.py
+++ src/gocept/selenium/wsgi/tests.py
@@ -13,15 +13,12 @@
 ##############################################################################
 
 import gocept.selenium.wsgi
-from gocept.selenium.wsgi.testing import SimpleApp
-
-
-test_layer = gocept.selenium.wsgi.Layer(SimpleApp())
+from gocept.selenium.wsgi.testing import SimpleApp, SimpleApp2
 
 
 class TestWSGITestCase(gocept.selenium.wsgi.TestCase):
 
-    layer = test_layer
+    layer = gocept.selenium.wsgi.Layer(SimpleApp())
 
     def test_wsgi_layer(self):
         self.assertTrue(self.layer.thread.isAlive)
@@ -29,3 +26,13 @@ class TestWSGITestCase(gocept.selenium.wsgi.TestCase):
     def test_simple_app(self):
         self.selenium.open('/')
         self.selenium.assertTextPresent('Hello from javascript')
+
+class TestWSGILayerName(gocept.selenium.wsgi.TestCase):
+    """ We introduce a new test layer with a different application.
+    The name of the application is used in the name of the layer in order
+    to have the testrunner distinguish the two layers."""
+
+    layer = gocept.selenium.wsgi.Layer(SimpleApp2())
+
+    def test_layer_name(self):
+        self.assertEquals(self.layer.__name__, 'Layer.SimpleApp2')
