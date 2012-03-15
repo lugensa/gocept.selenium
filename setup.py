fd1788a0c4846c2e41b4352b5474abd0fbe99749
Michael Howitz <mh@gocept.com>
2012-03-15 20:06:58 +0000

    Back to development: 0.13.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ except ImportError:
 
 setup(
     name='gocept.selenium',
-    version='0.13.1',
+    version='0.13.2dev',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     url='http://packages.python.org/gocept.selenium',
