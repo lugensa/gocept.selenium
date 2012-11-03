e30b48281a78146cc2ba77e07bb0b496f5228314
Wolfgang Schnerring <wosc@wosc.de>
2012-11-03 14:14:42 +0100

    Preparing release 1.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='gocept.selenium',
-    version='0.18.dev0',
+    version='1.0',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     url='http://pypi.python.org/pypi/gocept.selenium',
