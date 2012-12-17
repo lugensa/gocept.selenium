bfbed5f708baa7d5188b0eb332c68be33dcb1328
Wolfgang Schnerring <ws@gocept.com>
2012-12-17 18:52:57 +0100

    Re #8448: Extract zope.app.testing flavour

--- src/gocept/selenium/ztk/__init__.py
+++ src/gocept/selenium/ztk/__init__.py
@@ -12,55 +12,21 @@
 #
 ##############################################################################
 
-from zope.server.taskthreads import ThreadedTaskDispatcher
-import asyncore
 import gocept.selenium.base
-import gocept.selenium.selenese
-import threading
-import time
-import zope.app.server.wsgi
-import zope.app.testing.functional
-import zope.app.wsgi
+import gocept.httpserverlayer.zopeapptesting
 
 
-class Layer(gocept.selenium.base.Layer):
+class Layer(gocept.selenium.base.IntegrationBase,
+            gocept.httpserverlayer.zopeapptesting.Layer):
 
-    def setUp(self):
-        task_dispatcher = ThreadedTaskDispatcher()
-        task_dispatcher.setThreadCount(1)
-        db = zope.app.testing.functional.FunctionalTestSetup().db
-        self.http = zope.app.server.wsgi.http.create(
-            'WSGI-HTTP', task_dispatcher, db, port=self.port)
-        self.thread = threading.Thread(target=self.run_server)
-        self.thread.setDaemon(True)
-        self.thread.start()
-        time.sleep(0.025)
-        _, self.port = self.http.socket.getsockname()
-        super(Layer, self).setUp()
-
-    def tearDown(self):
-        self.running = False
-        self.thread.join()
-        super(Layer, self).tearDown()
-
-    def run_server(self):
-        self.running = True
-        while self.running:
-            asyncore.poll(0.1)
-        self.http.close()
+    def __init__(self, *bases):
+        name = self.make_layer_name(bases)
+        super(Layer, self).__init__(name=name, bases=bases)
 
 
 class TestCase(gocept.selenium.base.TestCase,
-               zope.app.testing.functional.FunctionalTestCase):
-    # note: MRO requires the gocept.selenium.base.TestCase to come first,
-    # otherwise setUp/tearDown happens in the wrong order
-
-    def setUp(self):
-        # switches the HTTP-server's database to the currently active
-        # DemoStorage (which is set by FunctionalTestCase)
-        super(TestCase, self).setUp()
-        db = zope.app.testing.functional.FunctionalTestSetup().db
-        application = self.layer.http.application
-        assert isinstance(application, zope.app.wsgi.WSGIPublisherApplication)
-        factory = type(application.requestFactory)
-        application.requestFactory = factory(db)
+               gocept.httpserverlayer.zopeapptesting.TestCase):
+    """NOTE: MRO requires gocept.selenium.base.TestCase to come first,
+    otherwise its setUp/tearDown is never called, since unittest.TestCase
+    does not call super().
+    """
