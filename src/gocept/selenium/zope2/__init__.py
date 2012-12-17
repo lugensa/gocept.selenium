a57e07f0f903ded3d5b8a421d611029c22cd96b8
Wolfgang Schnerring <wosc@wosc.de>
2012-12-17 20:45:47 +0100

    Extract Zope2TestCase flavour

--- src/gocept/selenium/zope2/__init__.py
+++ src/gocept/selenium/zope2/__init__.py
@@ -12,95 +12,21 @@
 #
 ##############################################################################
 
-import Lifetime
-import Testing.ZopeTestCase
-import Testing.ZopeTestCase.ZopeLite
-import Testing.ZopeTestCase.threadutils
-import Testing.ZopeTestCase.utils
-import ZServer.HTTPServer
-import Zope2
-import asyncore
+import gocept.httpserverlayer.zope2
 import gocept.selenium.base
-import time
 
 
-class Layer(gocept.selenium.base.Layer):
+class Layer(gocept.selenium.base.IntegrationBase,
+            gocept.httpserverlayer.zope2.Layer):
 
-    def setUp(self):
-        # adapted from Testing.ZopeTestCase.utils.startZServer() to make
-        # host/port configurable
-        Testing.ZopeTestCase.threadutils.setNumberOfThreads(1)
-        log = None
-        thread = Testing.ZopeTestCase.threadutils.QuietThread(
-            target=Testing.ZopeTestCase.threadutils.zserverRunner,
-            args=(self.host, self.port, log))
-        thread.setDaemon(True)
-        thread.start()
-
-        # Find actually bound port
-        i = 0
-        while not self.port and i < 1000:
-            i += 1
-            time.sleep(0.025)
-            for dispatcher in asyncore.socket_map.values():
-                if isinstance(dispatcher, ZServer.HTTPServer.zhttp_server):
-                    try:
-                        self.port = dispatcher.server_port
-                    except AttributeError:
-                        # Seems to happen when the dispatcher instance has been
-                        # created, but the port wasn't bound, yet. Just ignore
-                        # and wait for the next cycle.
-                        pass
-                    else:
-                        break
-
-        # notify ZopeTestCase infrastructure that a ZServer has been started
-        Testing.ZopeTestCase.utils._Z2HOST = self.host
-        Testing.ZopeTestCase.utils._Z2PORT = self.port
-
-        super(Layer, self).setUp()
-
-    def tearDown(self):
-        Lifetime.shutdown(0, fast=1)
-        super(Layer, self).tearDown()
-
-
-class SandboxPatch(object):
-    # Testing.ZopeTestCase.sandbox.Sandbox swapping of the DemoStorage is a
-    # little... crude:
-    #
-    # ZApplicationWrapper is instantiated with a DB from
-    # Testing/custom_zodb, which is never used later on, since Sandbox
-    # passes in the connection (to the current DB) to use instead. This
-    # connection is also stored globally in
-    # Testing.ZopeTestCase.sandbox.AppZapper (and passed to requests via
-    # the bobo_traverse monkey-patch there) -- which means that there only
-    # ever is one single ZODB connection, among the test code and the HTTP
-    # requests, and among concurrent requests. This clearly is not what we
-    # want.
-    #
-    # Thus, this rewrite of the upstream method, that properly changes the
-    # DB in ZApplicationWrapper and does *not* use AppZapper, yielding a
-    # new connection upon each traversal. (For reference and since it took
-    # me quite a while to figure out where everything is: this code is
-    # adapted from the original Sandbox._app and the normal Zope2 startup
-    # in Zope2.__init__).
-
-    def _app(self):
-        Zope2.startup()
-        stuff = Zope2.bobo_application._stuff
-        db = Testing.ZopeTestCase.ZopeLite.sandbox()
-        Zope2.bobo_application._stuff = (db,) + stuff[1:]
-        app = Zope2.bobo_application()
-        app = Testing.ZopeTestCase.utils.makerequest(app)
-        Testing.ZopeTestCase.connections.register(app)
-        return app
+    def __init__(self, *bases):
+        name = self.make_layer_name(bases)
+        super(Layer, self).__init__(name=name, bases=bases)
 
 
 class TestCase(gocept.selenium.base.TestCase,
-               SandboxPatch,
-               Testing.ZopeTestCase.FunctionalTestCase):
-
-    def getRootFolder(self):
-        """forward API-compatibility with zope.app.testing"""
-        return self.app
+               gocept.httpserverlayer.zope2.TestCase):
+    """NOTE: MRO requires gocept.selenium.base.TestCase to come first,
+    otherwise its setUp/tearDown is never called, since unittest.TestCase
+    does not call super().
+    """
