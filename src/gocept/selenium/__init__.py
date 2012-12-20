61d75743783e5b07a0eebb089259cc435d68fd67
Thomas Lotze <tl@gocept.com>
2012-12-20 12:23:42 +0100

    prevent webdriver imports with Python 2.5 as well (selenium.webdriver uses property setters)

--- src/gocept/selenium/__init__.py
+++ src/gocept/selenium/__init__.py
@@ -2,5 +2,5 @@ from util import skipUnlessBrowser
 from seleniumrc import Layer as RCLayer
 
 import sys
-if sys.version_info >= (2, 5):
+if sys.version_info >= (2, 6):
     from webdriver import Layer as WebdriverLayer
