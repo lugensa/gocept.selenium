f41b39e7a1d7180d9d6c693b2fa4cd9c9f1a2933
Thomas Lotze <tl@gocept.com>
2012-12-21 15:41:40 +0100

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='gocept.selenium',
-    version='1.1.2',
+    version='1.2.dev0',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     url='http://pypi.python.org/pypi/gocept.selenium',
