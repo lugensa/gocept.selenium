facdcabaaf3757b7af7f2765f21840a20f4dc080
Wolfgang Schnerring <wosc@wosc.de>
2013-01-09 10:22:20 +0100

    Preparing release 2.0.0a2

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='gocept.selenium',
-    version='1.1.2.dev0',
+    version='2.0.0a2',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     url='http://pypi.python.org/pypi/gocept.selenium',
