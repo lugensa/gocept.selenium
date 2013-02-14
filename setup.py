4548337531bc6625d88a723d64d51999fd1345f6
Daniel Havlik <dh@gocept.com>
2013-02-14 11:18:31 +0100

    Preparing release 2.0.0b1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='gocept.selenium',
-    version='2.0.0a3.dev0',
+    version='2.0.0b1',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     url='http://pypi.python.org/pypi/gocept.selenium',
