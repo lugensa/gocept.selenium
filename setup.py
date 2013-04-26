4a3447e508a6cda76699960689984a81f7fccd85
Sebastian Wehrmann <sw@gocept.com>
2013-04-26 10:24:47 +0200

    Preparing release 2.0.0b4

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='gocept.selenium',
-    version='2.0.0b4.dev0',
+    version='2.0.0b4',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     url='http://pypi.python.org/pypi/gocept.selenium',
