84bd56d67f2887c5db1f59093bc9380f87cc3a8b
Wolfgang Schnerring <ws@gocept.com>
2013-10-02 11:01:15 +0200

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='gocept.selenium',
-    version='2.0.0',
+    version='2.0.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     url='http://pypi.python.org/pypi/gocept.selenium',
