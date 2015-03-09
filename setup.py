a003de5ad7e87fb1f8faaf2e9a0f8b666f0dc2ad
Florian Pilz <fp@gocept.com>
2015-03-09 13:25:11 +0100

    Preparing release 2.3.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='gocept.selenium',
-    version='2.2.3.dev0',
+    version='2.3.0',
     author='Zope Foundation and Contributors',
     author_email='ws@gocept.com',
     url='https://goceptselenium.readthedocs.org/',
