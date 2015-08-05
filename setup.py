ee737b4220835f278da47b5facca2b334fe79edf
Michael Howitz <mh@gocept.com>
2015-08-05 21:18:34 +0200

    Back to development: 2.6

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name='gocept.selenium',
-    version='2.5.0',
+    version='2.6.dev0',
     author='Zope Foundation and Contributors',
     author_email='mail@gocept.com',
     url='https://goceptselenium.readthedocs.org/',
