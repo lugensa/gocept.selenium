4b7cbfbb4474c6a750b40868891d9dfe6f62c3a2
Michael Howitz <mh@gocept.com>
2016-04-13 17:12:37 +0200

    Currently only supporting a ``selenium`` version < 2.53 as this version breaks using a custom Firefox.
    See https://github.com/SeleniumHQ/selenium/issues/1965

--- src/gocept/selenium/webdriver.py
+++ src/gocept/selenium/webdriver.py
@@ -12,6 +12,7 @@
 #
 ##############################################################################
 
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 import ast
 import atexit
@@ -77,7 +78,7 @@ class Layer(plonetesting.Layer):
             parameters['firefox_profile'] = self.profile
         ff_binary = os.environ.get('GOCEPT_WEBDRIVER_FF_BINARY')
         if ff_binary:
-            parameters['firefox_binary'] = ff_binary
+            parameters['firefox_binary'] = FirefoxBinary(ff_binary)
         module = getattr(selenium.webdriver, self._browser)
         self['seleniumrc'] = module.webdriver.WebDriver(**parameters)
 
