664ff83bcbf3cba36225a25733f0b6b17e4221dd
Michael Howitz <mh@gocept.com>
2016-04-26 17:09:18 +0200

    Python < 2.7 is no longer supported.

--- src/gocept/selenium/__init__.py
+++ src/gocept/selenium/__init__.py
@@ -1,7 +1,4 @@
 from util import skipUnlessBrowser
 from seleniumrc import Layer as RCLayer, TestCase as RCTestCase
-
-import sys
-if sys.version_info >= (2, 6):
-    from webdriver import Layer as WebdriverLayer
-    from webdriver import WebdriverSeleneseLayer, WebdriverSeleneseTestCase
+from webdriver import Layer as WebdriverLayer  # noqa
+from webdriver import WebdriverSeleneseLayer, WebdriverSeleneseTestCase  # noqa
--- src/gocept/selenium/tests/__init__.py
+++ src/gocept/selenium/tests/__init__.py
@@ -1,14 +1,1 @@
 # python package
-
-import imp
-import sys
-import unittest
-
-
-if sys.version_info < (2, 6):
-    for name in (
-        'gocept.selenium.tests.test_webdriver',
-        'gocept.selenium.tests.test_wd_selenese',
-        ):
-        m = sys.modules[name] = imp.new_module(name)
-        m.test_suite = lambda: unittest.TestSuite()
