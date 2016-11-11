1317f276df29aa6cbb08d5d8eaa77f560200612e
Michael Howitz <mh@gocept.com>
2016-11-11 13:36:28 +0100

    Preparing release 3.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 
 setup(
     name='gocept.selenium',
-    version='3.1.dev0',
+    version='3.1',
     author='gocept and contributors',
     author_email='mail@gocept.com',
     url='https://goceptselenium.readthedocs.org/',
